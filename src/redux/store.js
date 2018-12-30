860b503d2814f5acae4fc8b8bea65cbb30a9dd7c
FrederickFoxxx <fwhduffield@gmail.com>
2018-12-30 16:31:45 +0000

    removed local storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,13 @@
 import { createStore } from 'redux';
 import todoApp from './reducers';
-import { loadState, saveState } from './localStorage';
-import throttle from 'lodash/throttle';
 
 const addLoggingToDispatch = store => {
+  /* eslint-disable no-console */
   const rawDispatch = store.dispatch;
   if (!console.group) {
     return rawDispatch;
   }
+
   return action => {
     console.group(action.type);
     console.log('%c prev state', 'color: gray', store.getState());
@@ -17,21 +17,17 @@ const addLoggingToDispatch = store => {
     console.groupEnd(action.type);
     return returnValue;
   };
+  /* eslint-enable no-console */
 };
 
 const configureStore = () => {
-  const persistedState = loadState();
-  const store = createStore(todoApp, persistedState);
+  const store = createStore(todoApp);
 
   if (process.env.NODE_ENV !== 'production') {
     store.dispatch = addLoggingToDispatch(store);
   }
 
-  store.subscribe(
-    throttle(() => {
-      saveState({ todos: store.getState().todos });
-    }, 1000)
-  );
   return store;
 };
+
 export default configureStore;
