5966fcadd1ad17ca7ab0016d1fd8800986ee192b
FrederickFoxxx <fwhduffield@gmail.com>
2018-12-30 16:36:53 +0000

    added fake databsase and fake api call

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './redux/store';
 import Root from './components/Root';
+import fetchTodos from './api/index';
+
+fetchTodos('all').then(todos => console.log(todos));
 
 const store = configureStore();
 
