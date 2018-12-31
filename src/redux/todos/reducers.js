817fc01a5fc6dd56f8d3be7efc15fd7e4fad759e
FrederickFoxxx <fwhduffield@gmail.com>
2018-12-31 09:02:01 +0000

    moved reducer code into reducers file

---
+++ src/redux/todos/reducers.js
@@ -0,0 +1,61 @@
+import { combineReducers } from 'redux';
+
+const byId = (state = {}, action) => {
+  switch (action.type) {
+    case 'RECIEVE_TODOS':
+      const nextState = { ...state };
+      action.response.forEach(todo => {
+        nextState[todo.id] = todo;
+      });
+      return nextState;
+    default:
+      return state;
+  }
+};
+
+const createList = filter => {
+  // eslint-disable-line arrow-body-style
+  const ids = (state = [], action) => {
+    if (filter !== action.filter) {
+      return state;
+    }
+    switch (action.type) {
+      case 'RECEIVE_TODOS':
+        return action.response.map(todo => todo.id);
+      default:
+        return state;
+    }
+  };
+
+  const isFetching = (state = false, action) => {
+    if (filter !== action.filter) {
+      return state;
+    }
+    switch (action.type) {
+      case 'REQUEST_TODOS':
+        return true;
+      case 'RECEIVE_TODOS':
+        return false;
+      default:
+        return state;
+    }
+  };
+
+  return combineReducers({
+    ids,
+    isFetching
+  });
+};
+
+const listByFilter = combineReducers({
+  all: createList('all'),
+  active: createList('active'),
+  completed: createList('completed')
+});
+
+const todos = combineReducers({
+  byId,
+  listByFilter
+});
+
+export default todos;
